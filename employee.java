021c292718e71c882bc43d258766de64187056a6
01fe22bca237 <01fe22bca237@kletech.ac.in>
2024-03-19 10:34:35 +0530

    person code

---
+++ employee.java
@@ -0,0 +1,23 @@
+public class employee  {
+    public int empid;
+    public int salary;
+    public employee(int empid, int salary){
+           this.empid=empid;
+           this.salary=salary;
+
+
+    }
+    public void display(){
+        System.out.println("empid is :" +empid);
+        System.out.println("salary is :" +salary);
+
+    }
+    public static void main(String[] args){
+       // person p =new person();
+        employee e = new employee(103, 25000);
+        // p.displayName();
+        // p.name="Sadhana";
+        // p.displayAge("21-09-2004");
+       e.display();
+    }
+} 
